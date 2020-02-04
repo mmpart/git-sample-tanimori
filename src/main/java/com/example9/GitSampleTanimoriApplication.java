bb3f03a2659ee294616ccca0bd381bf20e694a58
tanimorimizuki <mizuki.tanimori@rakus-partners.co.jp>
2020-02-04 12:16:01 +0900

    Carクラスを新規作成しました。

---
+++ src/main/java/com/example9/GitSampleTanimoriApplication.java
@@ -0,0 +1,13 @@
+package com.example9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSampleTanimoriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitSampleTanimoriApplication.class, args);
+	}
+
+}
